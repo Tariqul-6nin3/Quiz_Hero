0b3bca422155f2825c7931ed706f9d2fe1aeb07e
Tariqul <tariqularmsfitt018@gmail.com>
2023-03-15 21:39:24 +0600

    Handling the TypeError problem of QuizContainer

--- index.js
+++ index.js
@@ -46,13 +46,15 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
+
   displayQuiz(data);
 };
 
 // Displaying quiz on quiz page
 const displayQuiz = data => {
+  console.log(data);
   if (!data) {
     quizContainer.innerHTML = "";
     return;
